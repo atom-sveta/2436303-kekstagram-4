540c0626e0e4e5fed8821fc1e6223cc898a9a14b
Svetlana <mukhina.co@gmail.com>
2025-01-10 18:52:21 +0300

    Кексобукинг. Промежуточный вариант

--- js/main.js
+++ js/main.js
@@ -54,3 +54,45 @@ createObject();
 
 const objects = Array.from({length: 25},createObject);
 
+// Кексобукинг
+
+const bildingType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinArr = ['12:00', '13:00', '14:00'];
+const checkoutArr = ['12:00', '13:00', '14:00'];
+const feachersArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const randomInteger = getRandomInteger(1, 10);
+
+const padNum = (num) => num.toString().padStart(2, 0);
+
+const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
+
+const randomAbsValue = Math.abs(Math.round(Math.random() * 1000));
+
+ const createAd = () => {
+  return {
+  author: {avatar: `img/avatars/user${padNum(randomInteger)}.png`},
+
+  location: {
+    lat: getRandomArbitrary(35.65000, 35.70000),
+    lng: getRandomArbitrary(139.70000, 139,80000)
+  },
+
+  offer: {
+    title: 'Заголовок предложения',
+    address: {{location.lat}; {location.lng}},
+    price: randomAbsValue,
+    type: getRandomInteger(0, bildingType.length - 1),
+    rooms: randomAbsValue,
+    guests: randomAbsValue,
+    checkin: getRandomInteger(0, checkinArr.length - 1),
+    checkout: getRandomInteger(0, checkoutArr.length - 1),
+    features: [],
+    description: 'Описание помещения',
+    photos: []
+  },
+
+  }
+};
+
+createAd()
