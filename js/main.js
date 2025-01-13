8fa4bc5c6325c3e97abdd88dadda3069868b08cb
Svetlana <mukhina.co@gmail.com>
2025-01-13 21:14:59 +0300

    Кексаграмю Правки

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-// id опубликованной фотографии. Число от 1 до 25. Не должны повторяться
-// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Не должны повторяться
-// likes Случайное число от 15 до 200
-// Поле avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
-
-const NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-];
-
-const TEXT = [
+// Кексаграм
+const PICTURE_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_COUNT = 20;
+const COMMENT_LINES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const DESCRIPTIONS = [
+'Вау, ты выглядишь так красиво!',
+'Обалдеть, эти глаза, как жемчужины.',
+'Улыбка на миллион.',
+'Как у тебя получается всегда быть такой идеальной?',
+'Ты сногсшибательная!',
+'Нельзя быть такой совершенной!',
+'Это выглядит так красиво.',
+'Хотел бы я дважды поставить лайк.',
+];
+
+const NAMES = [
+  'Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон',
 ];
 
 const getRandomInteger = (a, b) => {
@@ -32,27 +36,48 @@ const getRandomInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-function createObject () {
+const createMessage = () => Array.from(
+{length: getRandomInteger(1, 2)},
+  () => getRandomArrayElement(COMMENT_LINES),
+).join(' ');
 
-  return {
-    id: getRandomInteger(1, 25),
-    url: `photos/${  getRandomInteger(1, 25)  }.jpg`,
-    description: 'Описание фотографии',
-    likes: getRandomInteger(15, 200),
-    comments: [
-      {
-        id: Math.round(Math.random() * 100),
-        avatar: `img/avatar-${getRandomInteger(1, 25)}.svg`,
-        message: getRandomArrayElement(TEXT),
-        name: getRandomArrayElement(NAMES),
-      },
-    ],
+const createIdGenerator = () => {
+  let lastGenerateId = 0;
+
+  return () => {
+    lastGenerateId += 1;
+    return lastGenerateId
   };
-}
+};
 
-createObject();
+const generateCommentId = createIdGenerator();
 
-const objects = Array.from({length: 25},createObject);
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPicture = (index) => ({
+    id: index,
+    url: `photos/${index}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+    comments: Array.from(
+      {length: getRandomInteger(0, COMMENT_COUNT)},
+      createComment,
+    ),
+});
+
+createPicture();
+
+const getPictures = () => Array.from(
+  {length: PICTURE_COUNT},
+  (_, pictureIndex) => createPicture(pictureIndex + 1),
+);
+
+console.log(getPictures())
 
 // Кексобукинг
 
@@ -67,7 +92,7 @@ const padNum = (num) => num.toString().padStart(2, 0);
 
 const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
 
-const randomAbsValue = Math.abs(Math.round(Math.random() * 1000));
+const randomAbsValue = Math.abs().Math.round(Math.random() * 1000);
 
  const createAd = () => {
   return {
@@ -80,7 +105,7 @@ const randomAbsValue = Math.abs(Math.round(Math.random() * 1000));
 
   offer: {
     title: 'Заголовок предложения',
-    address: {{location.lat}; {location.lng}},
+    address: {locationlat, locationlng},
     price: randomAbsValue,
     type: getRandomInteger(0, bildingType.length - 1),
     rooms: randomAbsValue,
