468edae5505b7417909dbc16684c22d120a16818
Svetlana <mukhina.co@gmail.com>
2025-03-28 17:27:08 +0300

    start effect

--- js/effect.js
+++ js/effect.js
@@ -1,26 +1,82 @@
 import { previewNode } from './scale.js';
 
 const effectsContainerNode = document.querySelector('.img-upload__effects');
-const sliderNode = document.querySelector('.effect-level__slider');
-const effectValueNode = ('.effect-level__value');
+const effectContainerNode = document.querySelector('.img-upload__effect-level');
+const sliderNode = effectContainerNode.querySelector('.effect-level__slider');
+const effectValueNode = effectContainerNode.querySelector('.effect-level__value');
 
-let prevEvent ;
+const effects = {
+  none: '',
+  chrome: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+    getCssFilter: (value) => `grayscale(${value})`
+  },
+  sepia: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1,
+    getCssFilter: (value) => `sepia(${value})`
+  },
+  marvin: {
+    MIN: 0,
+    MAX: 100,
+    STEP: 1,
+    getCssFilter: (value) => `invert(${value}%)`
+  },
+  phobos: {
+    MIN: 0,
+    MAX: 3,
+    STEP: 0.1,
+    getCssFilter: (value) => `blur(${value}px)`
+  },
+  heat: {
+    MIN: 1,
+    MAX: 3,
+    STEP: 0.1,
+    getCssFilter: (value) => `brightness(${value})`
+  },
+};
+
+let activeEffect = 'none';
+
+const getEffectOptions = (effectType) => {
+  console.log(effects[effectType]);
+  const {MIN, MAX, STEP} = effects[effectType];
+  return {
+    start: MAX,
+    connect: 'lower',
+    step: STEP,
+    range: {
+      'min': MIN,
+      'max': MAX
+    },
+  };
+};
+
+noUiSlider.create(sliderNode, getEffectOptions);
+
+// sliderNode.noUiSlider.on('update', () => {
+//   effectValueNode.value = sliderNode.noUiSlider.get();
+// });
+
+// let prevEvent ;
+// const selectEffect = (evt) => {
+//   previewNode.classList.remove(prevEvent);
+//   previewNode.classList.add(`effects__preview--${evt.target.value}`);
+//   prevEvent = `effects__preview--${evt.target.value}`;
+//   noUiSlider.create();
+// };
+
 const selectEffect = (evt) => {
-  previewNode.classList.remove(prevEvent);
-  previewNode.classList.add(`effects__preview--${evt.target.value}`);
-  prevEvent = `effects__preview--${evt.target.value}`;
+  effectValueNode.value = sliderNode.noUiSlider.get();
+  const value = evt.target.value;
+  console.log(value);
+  previewNode.style.filter = effects[value].getCssFilter(effectValueNode.value);
+  getEffectOptions(value);
 };
 
 effectsContainerNode.addEventListener('change', selectEffect);
 
 // const resetEffect = () => {};
-
-noUiSlider.create(sliderNode, {
-  range: {
-    'min': 0,
-    'max': 100
-  },
-  start: 100,
-  connect: 'lower',
-});
-
