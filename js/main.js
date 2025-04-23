6343f3ad5f675d50cc6abb4026a76739ba7d4e7f
Svetlana <mukhina.co@gmail.com>
2025-04-23 17:23:17 +0300

    Удален модуль scale из main

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ import {renderGallery} from './gallery.js';
 import {showAlert} from './util.js';
 import {closeOverlay} from './user-modal.js';
 import {setUserFormSubmit} from './user-form.js';
-import {resetScale} from './scale.js';
 import {addMessages, successMessageHandler, errorMessageHandler} from './message.js';
 import { getData, sendData } from './api.js';
 
-resetScale();
 addMessages();
 
 setUserFormSubmit(async (data) => {
